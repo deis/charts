8d7ace519e415fe9647173571301b181cf30d8a1
Seth Goings <sgoings@engineyard.com>
2016-02-19 15:30:43 -0700

    feat(_scripts): improve version bumper script
    So now you can do:
    
    CI=true LOGGER_GIT_SHA=<git hash for logger> make install
    
    And the chart manifests will be updated before `helm install`
    is called

--- _scripts/chart-version-bumper.sh
+++ _scripts/chart-version-bumper.sh
@@ -17,19 +17,25 @@ CHART_NAME="${1}"
 COMPONENT_NAME="${2}"
 VERSION="${3}"
 SHORT_VERSION="${VERSION:0:7}" # short git hashes (what we use in registries) are the 7 characters
+GIT_PREFIXED_TAG="git-${SHORT_VERSION}"
 
 # find the file where we reference the component
-target_file="$(grep -rl "image: quay.io/deisci/${COMPONENT_NAME}:*" ${CHART_NAME})"
+target_file="$(grep -Erl "image: quay.io/[a-z]+/${COMPONENT_NAME}:*" ${CHART_NAME})"
+
+echo "==> Operating on files: ${target_file}"
 
 # replace the version with VERSION
 file=""
 for file in ${target_file}; do
-  sed -i .bak "s%image: quay.io/deisci/${COMPONENT_NAME}:.*%image: quay.io/deisci/${COMPONENT_NAME}:${SHORT_VERSION}%" "${file}"
-  echo "${CHART_NAME}: set ${COMPONENT_NAME} to ${SHORT_VERSION}"
+  echo -e "\t${CHART_NAME}: set ${COMPONENT_NAME} to ${GIT_PREFIXED_TAG}"
+  sed -E -i .bak "s%image: quay.io/([a-z]+)/${COMPONENT_NAME}:.*%image: quay.io/\1/${COMPONENT_NAME}:${GIT_PREFIXED_TAG}%" "${file}"
 done
 
 # remove the backup file
 find . -name "*.bak" | xargs -I {} rm {}
 
 # print out the updated file just for kicks
-echo -e "\t$(grep -r "image: quay.io/deisci/${COMPONENT_NAME}:${SHORT_VERSION}" ${target_file})"
+echo
+echo -e "\tVerification:"
+echo -e "\t\t$(grep -Er "image: quay.io/[a-z]+/${COMPONENT_NAME}:${GIT_PREFIXED_TAG}" ${target_file})"
+echo
