84ff6af373e387a6cbd1d5f49650c64891a3649a
Seth Goings <sgoings@engineyard.com>
2016-02-19 15:30:43 -0700

    feat(_scripts): improve version bumper script
    So now you can do:
    
    CI=true LOGGER_GIT_SHA=<git hash for logger> make install
    
    And the chart manifests will be updated before `helm install`
    is called

--- _scripts/tests/install.sh
+++ _scripts/tests/install.sh
@@ -5,9 +5,42 @@ cd "$(dirname $0)"
 source config.sh
 source shared.sh
 
+function bumpver-if-set {
+  local chart="${1}"
+  local component="${2}"
+  local version="${3}"
+
+  if [ ! -z "${version}" ]; then
+    local version_bumper="_scripts/chart-version-bumper.sh"
+    "${version_bumper}" "${chart}" "${component}" "${version}"
+  else
+    echo "No version set for ${chart}: ${component}"
+  fi
+}
+
+if [ ! -z $CI ]; then
+  echo "INJECTING VERSIONS!"
+  echo "-------------------"
+  pushd ../../ &> /dev/null
+  bumpver-if-set "deis-logger" "logger" "${LOGGER_GIT_SHA}"
+  bumpver-if-set "deis-dev" "builder" "${BUILDER_GIT_SHA}"
+  bumpver-if-set "deis-dev" "minio" "${MINIO_GIT_SHA}"
+  bumpver-if-set "deis-dev" "postgres" "${POSTGRES_GIT_SHA}"
+  bumpver-if-set "deis-dev" "registry" "${REGISTRY_GIT_SHA}"
+  bumpver-if-set "deis-dev" "router" "${ROUTER_GIT_SHA}"
+  bumpver-if-set "deis-dev" "workflow" "${WORKFLOW_GIT_SHA}"
+  popd &> /dev/null
+  echo "-------------------"
+fi
+
 rm -rf ~/.helm/
-helm repo add deis https://github.com/deis/charts
-helm up
+helm doctor
+mkdir -p ~/.helm/cache/charts/deis
+rsync -av ../../ ${HOME}/.helm/cache/deis/ > /dev/null
 helm fetch deis/deis-dev
+helm fetch deis/deis-logger
+
+test_root="$(dirname $(pwd))"
+
 helm generate deis-dev
 helm install deis-dev
