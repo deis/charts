648ae39a57ff10e5defc33711dfa40aa817633a2
Vaughn Dice <vdice@deis.com>
2016-03-25 11:22:00 -0600

    feat(jobs): add workflow-test job DSL

--- jobs/component_jobs.groovy
+++ jobs/component_jobs.groovy
@@ -1,31 +1,4 @@
-repos = [
-  [name: 'builder', slackChannel: 'builder'],
-  [name: 'dockerbuilder', slackChannel: 'builder'],
-  [name: 'fluentd', slackChannel: 'logger'],
-  [name: 'logger', slackChannel: 'logger'],
-  [name: 'minio', slackChannel: 'object-store'],
-  [name: 'postgres', slackChannel: 'postgres'],
-  [name: 'registry', slackChannel: 'registry'],
-  [name: 'router', slackChannel: 'router'],
-  [name: 'slugbuilder', slackChannel: 'builder'],
-  [name: 'slugrunner', slackChannel: 'builder'],
-  [name: 'controller', slackChannel: 'controller'],
-  [name: 'workflow-e2e', slackChannel: 'testing'],
-  [name: 'workflow-manager', slackChannel: 'wfm'],
-]
-
-repos.each { Map repo ->
-  repo.commitEnvVar = "${repo.name.toUpperCase().replaceAll('-', '_')}_SHA"
-}
-
-defaults = [
-  numBuildsToKeep: 42,
-  bumpverCommitCmd: 'git commit -a -m "chore(versions): ci bumped versions via ${BUILD_URL}" || true',
-  testJob: [master: 'workflow-test', pr: 'workflow-test-pr'],
-]
-
-// TODO: place the above ^^ into common.groovy
-// evaluate(new File("./common.groovy"))
+evaluate(new File("${WORKSPACE}/jobs/common.groovy"))
 
 repos.each { Map repo ->
   [
@@ -40,7 +13,7 @@ repos.each { Map repo ->
 
     job(name) {
       description """<ol>
-      <li>Watches the ${repo.name} repo for a ${config.type} commit/li>
+      <li>Watches the ${repo.name} repo for a ${config.type} commit</li>
       <li>Kicks off downstream ${defaults.testJob[config.type]} job to vet changes</li>
     </ol>"""
       scm {
@@ -89,7 +62,6 @@ repos.each { Map repo ->
             //       completedStatus('ERROR', 'Something went really wrong. Investigate!')
             //     }
             //   }
-            }
           }
         }
       }
