0b5366b2749930076334651d85172d4538b13189
Seth Goings <sgoings@engineyard.com>
2016-03-31 23:46:32 -0600

    fix(*): Jenkins sets SUDO_USER which messes with rerun behavior now

--- ci.sh
+++ ci.sh
@@ -4,12 +4,11 @@ set -eo pipefail
 
 rm -rf ${HOME}/.chart-mate
 
-export CI=true
-
 trap "./rerun chart-mate:down" EXIT
 
 _scripts/tests/install-chart-mate.sh
 
+unset SUDO_USER # https://github.com/rerun/rerun/issues/222
 ./rerun chart-mate:init
 ./rerun chart-mate:up
 ./rerun chart-mate:bumpver
