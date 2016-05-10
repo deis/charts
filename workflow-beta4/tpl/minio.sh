c1e07f8f2c28fa4a30cc36d183c42acdc01d1031
Vaughn Dice <vdice@deis.com>
2016-05-10 11:40:54 -0600

    chore(workflow-beta4): releasing workflow-beta4(-e2e)

---
+++ workflow-beta4/tpl/minio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  if [[ $line == "storage"* ]]; then
+    IFS='=' read -a storage <<< "$line"
+    if [ ${storage[1]} == "\"minio\"" ]; then
+      helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-minio-rc.yaml $HELM_GENERATE_DIR/tpl/deis-minio-rc.yaml
+      cp $HELM_GENERATE_DIR/tpl/deis-minio-service* $HELM_GENERATE_DIR/manifests/
+    else
+      rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
+    fi
+  fi
+done < "$HELM_GENERATE_DIR/tpl/generate_params.toml"
