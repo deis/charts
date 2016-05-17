33fbe2700c25563fedd5261849f43aa1f85ad33f
smothiki <smothiki@engineyard.com>
2016-05-16 18:15:29 -0600

    feat(tpl): template objectstorage creds through env variables

--- workflow-dev/tpl/minio.sh
+++ workflow-dev/tpl/minio.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-while IFS='' read -r line || [[ -n "$line" ]]; do
-  if [[ $line == "storage"* ]]; then
-    IFS='=' read -a storage <<< "$line"
-    if [ ${storage[1]} == "\"minio\"" ]; then
-      helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-minio-rc.yaml $HELM_GENERATE_DIR/tpl/deis-minio-rc.yaml
-      cp $HELM_GENERATE_DIR/tpl/deis-minio-service* $HELM_GENERATE_DIR/manifests/
-    else
-      rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
-    fi
+store=`grep "storage = " $HELM_GENERATE_DIR/tpl/generate_params.toml |cut -d "=" -f2 | tr -dc '[:alnum:]\n\r'`
+if [ -n "$STORAGE_TYPE" ]; then
+  if [ "$STORAGE_TYPE" != "minio" ]; then
+    rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
   fi
-done < "$HELM_GENERATE_DIR/tpl/generate_params.toml"
+elif [ "$store" != "minio" ]; then
+  rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
+else
+  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-minio-rc.yaml $HELM_GENERATE_DIR/tpl/deis-minio-rc.yaml
+  cp $HELM_GENERATE_DIR/tpl/deis-minio-service* $HELM_GENERATE_DIR/manifests/
+fi
