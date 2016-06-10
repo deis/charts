5b29b4cd5da48d4b9f7761d2b3239f890e8cbdf3
Kent Rancourt <kent@deis.com>
2016-06-10 14:12:46 -0400

    feat(database): Add easier off-cluster db config

---
+++ workflow-dev/tpl/storage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+store=`grep "storage\s*=\s*" $HELM_GENERATE_DIR/tpl/generate_params.toml | cut -d "=" -f2 | tr -dc '[:alnum:]\n\r'`
+if [ -n "$STORAGE_TYPE" ]; then
+  if [ "$STORAGE_TYPE" != "minio" ]; then
+    rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
+  fi
+elif [ "$store" != "minio" ]; then
+  rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
+else
+  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-minio-rc.yaml $HELM_GENERATE_DIR/tpl/deis-minio-rc.yaml
+  cp $HELM_GENERATE_DIR/tpl/deis-minio-service* $HELM_GENERATE_DIR/manifests/
+fi
+
+database_location=`grep "database_location\s*=\s*" $HELM_GENERATE_DIR/tpl/generate_params.toml | cut -d "=" -f2 | tr -dc '[:alnum:]-\n\r'`
+if [ "$DATABASE_LOCATION" = "off-cluster" ] || [ "$database_location" = "off-cluster" ]; then
+  rm -rf $HELM_GENERATE_DIR/manifests/deis-database-*
+else
+  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-database-rc.yaml $HELM_GENERATE_DIR/tpl/deis-database-rc.yaml
+  cp $HELM_GENERATE_DIR/tpl/deis-database-service* $HELM_GENERATE_DIR/manifests/
+fi
+helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-database-secret-creds.yaml $HELM_GENERATE_DIR/tpl/deis-database-secret-creds.yaml
