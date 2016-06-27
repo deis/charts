64b4512f8f36d025929ea6b8e0e5954328be526f
Keerthan Mala <kmala@engineyard.com>
2016-06-27 17:16:50 -0600

    fix(minio):create minio when using storage type as minio via env vars

--- workflow-dev/tpl/storage.sh
+++ workflow-dev/tpl/storage.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 store=`grep "storage\s*=\s*" $HELM_GENERATE_DIR/tpl/generate_params.toml | cut -d "=" -f2 | tr -dc '[:alnum:]\n\r'`
-if [ -n "$STORAGE_TYPE" ]; then
-  if [ "$STORAGE_TYPE" != "minio" ]; then
-    rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
-  fi
-elif [ "$store" != "minio" ]; then
-  rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
-else
+if [ "$STORAGE_TYPE" = "minio" ] || ( [ -z "$STORAGE_TYPE" ] && [ "$store" = "minio" ] ); then
   helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-minio-rc.yaml $HELM_GENERATE_DIR/tpl/deis-minio-rc.yaml
   cp $HELM_GENERATE_DIR/tpl/deis-minio-service* $HELM_GENERATE_DIR/manifests/
+else
+  rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
 fi
 
 database_location=`grep "database_location\s*=\s*" $HELM_GENERATE_DIR/tpl/generate_params.toml | cut -d "=" -f2 | tr -dc '[:alnum:]-\n\r'`
