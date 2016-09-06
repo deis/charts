c12210e51ccebaa52fcd96e3eced8223899ddccf
Keerthan Reddy Mala (kmala) <keerthan.mala@gmail.com>
2016-09-06 16:22:38 -0600

    feat(deployments): move components to use deployments instead of RC

--- workflow-dev/tpl/storage.sh
+++ workflow-dev/tpl/storage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 store=`grep "storage\s*=\s*" $HELM_GENERATE_DIR/tpl/generate_params.toml | cut -d "=" -f2 | tr -dc '[:alnum:]\n\r'`
 if [ "$STORAGE_TYPE" = "minio" ] || ( [ -z "$STORAGE_TYPE" ] && [ "$store" = "minio" ] ); then
-  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-minio-rc.yaml $HELM_GENERATE_DIR/tpl/deis-minio-rc.yaml
+  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-minio-deployment.yaml $HELM_GENERATE_DIR/tpl/deis-minio-deployment.yaml
   cp $HELM_GENERATE_DIR/tpl/deis-minio-service* $HELM_GENERATE_DIR/manifests/
 else
   rm -rf $HELM_GENERATE_DIR/manifests/deis-minio-*
@@ -9,7 +9,7 @@ fi
 
 database_location=`grep "database_location\s*=\s*" $HELM_GENERATE_DIR/tpl/generate_params.toml | cut -d "=" -f2 | tr -dc '[:alnum:]-\n\r'`
 if [ "$DATABASE_LOCATION" = "on-cluster" ] || ( [ -z "$DATABASE_LOCATION" ] && [ "$database_location" = "on-cluster" ] ); then
-  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-database-rc.yaml $HELM_GENERATE_DIR/tpl/deis-database-rc.yaml
+  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-database-deployment.yaml $HELM_GENERATE_DIR/tpl/deis-database-deployment.yaml
   cp $HELM_GENERATE_DIR/tpl/deis-database-service* $HELM_GENERATE_DIR/manifests/
 else
   rm -rf $HELM_GENERATE_DIR/manifests/deis-database-*
@@ -18,7 +18,7 @@ helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/m
 
 logger_redis_location=`grep "logger_redis_location\s*=\s*" $HELM_GENERATE_DIR/tpl/generate_params.toml | cut -d "=" -f2 | tr -dc '[:alnum:]-\n\r'`
 if [ "$LOGGER_REDIS_LOCATION" = "on-cluster" ] || ( [ -z "$LOGGER_REDIS_LOCATION" ] && [ "$logger_redis_location" = "on-cluster" ] ); then
-  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-logger-redis-rc.yaml $HELM_GENERATE_DIR/tpl/deis-logger-redis-rc.yaml
+  helmc tpl -d $HELM_GENERATE_DIR/tpl/generate_params.toml -o $HELM_GENERATE_DIR/manifests/deis-logger-redis-deployment.yaml $HELM_GENERATE_DIR/tpl/deis-logger-redis-deployment.yaml
   cp $HELM_GENERATE_DIR/tpl/deis-logger-redis-service* $HELM_GENERATE_DIR/manifests/
 else
   rm -rf $HELM_GENERATE_DIR/manifests/deis-logger-redis-*
